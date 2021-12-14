d9dfdc9f6a356f692e18865caea8951413f68cc2
nebula <yzh.nebula@foxmail.com>
2021-12-14 10:20:36 +0800

    测试

---
+++ java_interview/src/interview1/Test6.java
@@ -0,0 +1,18 @@
+package interview1;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * @author Nebula
+ * @date 2021/12/14 9:44
+ * @description: TODO
+ */
+public class Test6 {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap<>();
+        CopyOnWriteArraySet copyOnWriteArraySet = new CopyOnWriteArraySet<>();
+    }
+}
