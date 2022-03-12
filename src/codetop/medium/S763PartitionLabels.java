803afd7fee5413f2544559630a75828a80c10538
yzhnebula <yzh.nebula@foxmail.com>
2022-03-12 14:28:52 +0800

    feat:s763区间划分

--- src/codetop/medium/S763PartitionLabels.java
+++ src/codetop/medium/S763PartitionLabels.java
@@ -28,6 +28,19 @@ public class S763PartitionLabels {
      * 贪心
      * 美团笔试
      *
+     * 由于同一个字母只能出现在同一个片段，显然同一个字母的第一次出现的下标位置
+     * 和最后一次出现的下标位置必须出现在同一个片段。
+     * 因此需要遍历字符串，得到每个字母最后一次出现的下标位置。
+     * 在得到每个字母最后一次出现的下标位置之后，可以使用贪心的方法将字符串划分为尽可能多的片段，
+     * 具体做法如下:
+     * 1.从左到右遍历字符串，遍历的同时维护当前片段的开始下标 {start}和结束下标 {end}，初始时 {start}={end}=0。
+     * 2.对于每个访问到的字母 c，得到当前字母的最后一次出现的下标位置 {end}_c，
+     * 则当前片段的结束下标一定不会小于 {end}_c，因此令 {end}=max({end},{end}_c)。
+     * 3.当访问到下标 {end} 时，当前片段访问结束，当前片段的下标范围是 [{start},{end}][start,end]，
+     * 长度为 {end}-{start}+1end−start+1，将当前片段的长度添加到返回值，然后令 {start}={end}+1start=end+1，
+     * 继续寻找下一个片段。
+     * 4.重复上述过程，直到遍历完字符串。
+     *
      * @param s
      * @return
      */
@@ -41,12 +54,14 @@ public class S763PartitionLabels {
 
         // 双指针
         int start = 0, end = 0;
+        //遍历字符串，每次更新当前分区的end值
         for (int i = 0; i < s.length(); i++) {
-            //更新end
+            //更新分区end值
             end = Math.max(end, lastCharPos[s.charAt(i) - 'a']);
-            //当最后位置值等于字符当前字符的位置，加入res
+            //当分区最后位置值等于当前字符的位置，即为一个分区加入res
             if (end == i) {
                 res.add(end - start + 1);
+                //下一个分区
                 start = end + 1;
             }
         }
