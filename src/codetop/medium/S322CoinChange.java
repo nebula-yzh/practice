2adf696d7801bb75c3ffab17e30c51bc538f6aa7
nebula <yzh.nebula@foxmail.com>
2022-03-15 16:54:15 +0800

    feat:Coin-Change

--- src/codetop/medium/S322CoinChange.java
+++ src/codetop/medium/S322CoinChange.java
@@ -1,18 +1,20 @@
-package leetcode;
+package codetop.medium;
 
 import java.util.Arrays;
 
 /**
- * leetcode 零钱兑换 中等
+ * @author nebula
+ * @date 2022/03/15 16:33
+ * 322 零钱兑换 中等
  *
  * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
  *
  * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回-1 。
  *
  * 你可以认为每种硬币的数量是无限的。
- * @author 应志豪
+
  */
-public class Solution322 {
+public class S322CoinChange {
 
     public static void main(String[] args) {
         int[] coins=new int[]{1,2,5};
@@ -33,6 +35,7 @@ public class Solution322 {
     /**
      * memo表示凑成对应金额所需的最少硬币数，比如3，最少要两个硬币
      * 因此初始化大小为amount+1
+     * 递归动态规划
      * @param coins
      * @param amount
      * @return
@@ -61,5 +64,26 @@ public class Solution322 {
         //把计算结果存入备忘录
         memo[amount] = (res == Integer.MAX_VALUE) ? -1 : res;
         return memo[amount];
+    }
+
+    /**
+     * 非递归动态规划
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange2(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
