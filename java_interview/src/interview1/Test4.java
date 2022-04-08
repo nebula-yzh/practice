400bc272f20b44529b1a15deeb5363a65abb4f35
nebula <yzh.nebula@foxmail.com>
2022-04-08 11:20:44 +0800

    test

--- java_interview/src/interview1/Test4.java
+++ java_interview/src/interview1/Test4.java
@@ -5,16 +5,16 @@ package interview1;
  * @date 2021/11/30 19:41
  * @description: TODO
  */
-public class Tset4 {
+public class Test4 {
     public static void main(String[] args) {
         int i = 10;
         long l = 128L;
         double d = 10D;
-        System.out.println(i==l);
+        System.out.println(i == l);
 
-            System.out.println();
+        System.out.println();
 
-        if (i==d){
+        if (i == d) {
             System.out.println(true);
         }
 
---
+++ java_interview/src/interview1/Test7.java
@@ -0,0 +1,17 @@
+package interview1;
+
+/**
+ * @author Nebula
+ * @date 2022/4/8 11:12
+ * @description: TODO
+ */
+public class Test7 {
+    public static void main(String[] args) {
+        String hex = Integer.toHexString(13);
+        String binaryString = Integer.toBinaryString(13);
+        String octalString = Integer.toOctalString(8);
+        System.out.println(hex);
+        System.out.println(binaryString);
+        System.out.println(octalString);
+    }
+}
